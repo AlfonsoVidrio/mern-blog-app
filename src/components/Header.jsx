646588dac0d7ee7cdea244d616f9e70e95b5b1fd
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-06-11 02:04:02 -0600

    estilos generales

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,25 @@
+import { Link } from 'react-router-dom';
+import Logo from '../images/logo.jpeg'
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+        <div className="container nav__container">
+            <Link to="/" className='nav__logo'>
+                <img src={Logo} alt="Navbar Logo" />
+            </Link>
+            <ul className='nav__menu'>
+                <li><Link to="/profile">Ernest Archiver</Link></li>
+                <li><Link to="/create">Crear publicación</Link></li>
+                <li><Link to="/authors">Autores</Link></li>
+                <li><Link to="/profile">cerrar sesión</Link></li>
+            </ul>
+            <button className='nav__toggle-btn'>
+                <AiOutlineClose />
+            </button>
+        </div>
+    </nav>
   )
 }
